f163fd34e73b868f058b36b011872be0f97dc19a
belen <bcerro245@gmail.com>
2023-04-23 17:48:51 +0200

    nuevo scrpt

---
+++ root1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#script para crear un usuario
+
+#primero comprobamos que somos el usuario root
+# echo ${UID} = indica cual es el UID del usuario
+#validarse como root 'sudo su'
+
+if [[ ${UID} -ne 0 ]] 
+then
+echo "Usage ${0}"
+echo
+echo"Tienes que ser root"
+exit 1
+fi
+
+read -p "Introduce el nombre de usuario: " USER_NAME
+
+echo "creando usuario ${USER_NAME}"
