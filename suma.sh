2fb33ea9829a08dd4e6ec23ae750c13505b01a2c
belen <bcerro245@gmail.com>
2023-04-24 09:32:02 +0200

    clse 2

--- suma.sh
+++ suma.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+#control de error
+# si no nos pasan dos parámetros damos mensaje de error y salimos del script
+NUM_PARAM=${#}
+#echo "NUM_PARA ${NUM_PARAM}"
+if [[ ${NUM_PARAM} -ne 2 ]]
+then
+    echo "ERROR: tienes que introducir dos parámetros"
+    exit 1
+fi
+
+# este script tiene que recoger dos parámetros, los tiene
+# que sumar y tiene que escribir en pantalla "a+b=resultado"
 
 A=${1}
-echo "A és ${A}"
 B=${2}
-echo "B és ${B}"
-C=${3}
-echo "C és ${C}"
-SUMA=$((A+B+C))
-echo "${A}+${B}+${C}=${SUMA}"
+SUMA=$((A+B))
+echo "${A}+${B}=${SUMA}"
