4235b3ce274fcd364cc21d1690837fde4004b563
belen <bcerro245@gmail.com>
2023-04-28 22:29:24 +0200

    no funciona como deberia

--- crear_usuarios.sh
+++ crear_usuarios.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-#script para crear un usuario
-#primero comprobamos que somos el usuario root
 function usage(){
     echo "Usage ${0}"
     echo
     echo "Tienes que ser root"
-    echo "el usuario no se puede repetir"
-    exit 1 #indica que hay un error
-}
 
+    exit 1
+}
+#script para crear un usuario
+
+#primero comprobamos que somos el usuario root
 # echo ${UID} = indica cual es el UID del usuario
 #validarse como root 'sudo su'
+
+if [[ ${UID} -ne 0 ]] 
+then
+    usage
+fi
+
+read -p "Introduce el nombre de usuario: " USER_NAME
+
+echo "creando usuario ${USER_NAME}"
 
 if [[ ${UID} -ne 0 ]] 
 then
@@ -44,7 +53,7 @@ then
 fi
 
 #hacer que caduque la contraseña
-password -e ${USER_NAME}
+passwd -e ${USER_NAME}
 
 
 exit 0 #indica que el script acaba bien
