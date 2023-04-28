01f1757d410c98b15b03407292d9896a7eeb106d
belen <bcerro245@gmail.com>
2023-04-28 23:15:07 +0200

    penultimo ejercicio

---
+++ param.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function usage (){
+      echo 
+    echo "USAGE: ${0} USER_NAME [COMMENTS]"
+    echo "COMMENTS contiene el nombre  completo"
+    exit 1
+}
+NUMBER_PARAM=${#}
+if [[ NUMBER_PARAM -eq 0 ]]
+then 
+   usage
+fi
+#OBLIGAR A QUE MÍNIMO SE TENGA QUE INTRODUCIR UN PARÁMETRO
+
+#aprenderemos a tratar con los argumentos (como se llaman fuera)
+#y parámetros (como se llaman dentro)
+SCRIPT_NAME=${0}
+echo "Nombre del script ${SCRIP_NAME}"
+#${1}.....${9}
+echo ¨primer parámetro: ${1}¨
+USER_NAME=${1}
+echo ¨USER_NAME: ${USER_NAME}¨
+echo ¨segundo parámetro: ${2}¨
