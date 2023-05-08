f482335080af365bedc673498e852ad869e32052
belen <bcerro245@gmail.com>
2023-05-08 20:50:10 +0200

    liadas de gitignore

--- telegram_bot_2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TOKEN="6110004324:AAFqdvnklVC1qz8LC25tPVkaLEGg6y2mkgg"
-ID="5047375565"
-MENSAJE=${1}
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-
-curl -s -X POST $URL -d chat_id=$ID -d text="$MENSAJE"
