680b0f31c14ef2ee2c5f9f03b5c12f9e3763ff83
belen <bcerro245@gmail.com>
2023-05-15 09:16:51 +0200

    mes scrpts clase

---
+++ exercici3_SO.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Son les $(date +"%H:%Mh")"
---
+++ exercici5_SO.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "El contingut del directori temporal /tmp es:"
+echo $(ls  /tmp)
+
+#EXER_6: crear dos directoris a /tmp. proves i copies
